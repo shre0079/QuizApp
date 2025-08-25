8a4c845f8796860eaf9a101c44a6ba78ccede97a
Shreyash <shreyashshree0079@gmail.com>
2025-08-25 23:35:10 +0530

    autowiring

--- src/main/java/Com/Shre/QuizApp/service/QuestionService.java
+++ src/main/java/Com/Shre/QuizApp/service/QuestionService.java
@@ -8,7 +8,8 @@ import org.springframework.stereotype.Service;
 @Service
 public class QuestionService {
 
-    public String getAllQuestions(){
+    @Autowired
+    private QuestionDao questionDao;
 
     }
 }
