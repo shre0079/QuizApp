e73d1c7b00e720bd8c6312055af4e6dd5ede0e20
Shreyash <shreyashshree0079@gmail.com>
2025-08-25 22:48:02 +0530

    first commit

---
+++ src/main/java/Com/Shre/QuizApp/QuestionController.java
@@ -0,0 +1,15 @@
+package Com.Shre.QuizApp;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("question")
+public class QuestionController {
+
+    @GetMapping("allQuestions")
+    public String getAllQuestions() {
+        return "Here are your questions";
+    }
+}
