3a4bbaa34035b2d94c8ae0b9070593347b07eccd
Shreyash <shreyashshree0079@gmail.com>
2025-08-25 23:35:00 +0530

    imports

--- src/main/java/Com/Shre/QuizApp/service/QuestionService.java
+++ src/main/java/Com/Shre/QuizApp/service/QuestionService.java
@@ -1,5 +1,8 @@
 package com.shre.quizApp.service;
 
+import com.shre.quizApp.dao.QuestionDao;
+import com.shre.quizApp.model.Question;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
