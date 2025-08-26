31ba83a64a47e4dee20e3f0f4f32ad63bc981a55
Shreyash <shreyashshree0079@gmail.com>
2025-08-26 19:58:15 +0530

    Jquery for fetching random ques

--- src/main/java/Com/Shre/QuizApp/dao/QuestionDao.java
+++ src/main/java/Com/Shre/QuizApp/dao/QuestionDao.java
@@ -13,5 +13,5 @@ public interface QuestionDao extends JpaRepository<Question, Integer> {
     List<Question> findByCategory(String category);
 
     @Query(value = "SELECT q.id FROM question q Where q.category=:category ORDER BY RANDOM() LIMIT :numQ", nativeQuery = true)
-    List<Integer> findRandomQuestionsByCategory(String category, int numQ);
+    List<Question> findRandomQuestionsByCategory(String category, int numQ);
 }
