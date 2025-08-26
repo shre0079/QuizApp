a4c682016d68c25cd47ca10083e3cd18cc58271d
Shreyash <shreyashshree0079@gmail.com>
2025-08-26 19:57:40 +0530

    get quiz question

--- src/main/java/Com/Shre/QuizApp/controller/QuestionController.java
+++ src/main/java/Com/Shre/QuizApp/controller/QuestionController.java
@@ -36,7 +36,7 @@ public class QuestionController {
     }
 
     @GetMapping("generate")
-    public ResponseEntity<List<Integer>> getQuestionsForQuiz
+    public ResponseEntity<List<Question>> getQuestionsForQuiz
             (@RequestParam String categoryName, @RequestParam Integer numQuestions ){
         return questionService.getQuestionsForQuiz(categoryName, numQuestions);
     }
