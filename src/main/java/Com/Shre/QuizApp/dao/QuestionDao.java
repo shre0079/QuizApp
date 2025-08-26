418f19ef379210303558a2c0c871fd3bfaa8a2a5
Shreyash <shreyashshree0079@gmail.com>
2025-08-26 20:20:51 +0530

    query update in quesdAO

--- src/main/java/Com/Shre/QuizApp/dao/QuestionDao.java
+++ src/main/java/Com/Shre/QuizApp/dao/QuestionDao.java
@@ -3,6 +3,7 @@ package com.shre.quizApp.dao;
 import com.shre.quizApp.model.Question;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import java.util.List;
 
@@ -12,6 +13,7 @@ public interface QuestionDao extends JpaRepository<Question, Integer> {
 
     List<Question> findByCategory(String category);
 
-    @Query(value = "SELECT q.id FROM question q Where q.category=:category ORDER BY RANDOM() LIMIT :numQ", nativeQuery = true)
-    List<Question> findRandomQuestionsByCategory(String category, int numQ);
+    @Query(value = "SELECT * FROM question q WHERE q.category = :category ORDER BY RANDOM() LIMIT :numQ", nativeQuery = true)
+    List<Question> findRandomQuestionsByCategory(@Param("category") String category, @Param("numQ") int numQ);
+
 }
